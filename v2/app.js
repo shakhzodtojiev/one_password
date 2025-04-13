08e6c209753fca766269b3b4e1483380e11485d3
shakhzodtojiev <shakhzodtojievuz@gmail.com>
2025-04-13 20:32:03 +0500

    Kod

---
+++ v2/app.js
@@ -0,0 +1,18 @@
+let one_password_list = [
+    ['id', 'app_name', 'username', 'password'],
+    ['1', 'Gmail', 'user@gmail.com', 'Pa2s@W0rd#'],
+    ['2', 'GitHub', 'gitusername', 'G1tPa2s@W0rd#'],
+    ['3', 'LinkedIn', 'linkeduser', 'l1nked1NPa2s@W0rd#'],
+    ['4', 'Medium', 'miduser', 'M1dPa2s@W0rd#'],
+    ['5', 'Instagram', 'instauser','1nstaPa2s@W0rd#'],
+    ['6', 'TikTok', 'tikuser', 'T1kT0kPa2s@W0rd#'],
+];
+
+let one_password_object = [
+    { id: '1', app_name: 'Gmail', username: 'user@gmail.com', password: 'Pa2s@W0rd#' },
+    { id: '2', app_name: 'GitHub', username: 'gitusername', password: 'G1tPa2s@W0rd#' },
+    { id: '3', app_name: 'LinkedIn', username: 'linkeduser', password: 'l1nked1NPa2s@W0rd#' },
+    { id: '4', app_name: 'Medium', username: 'miduser', password: 'M1dPa2s@W0rd#' },
+    { id: '5', app_name: 'Instagram', username: 'instauser', password: '1nstaPa2s@W0rd#' },
+    { id: '6', app_name: 'TikTok', username: 'tikuser', password: 'T1kT0kPa2s@W0rd#' }
+]
